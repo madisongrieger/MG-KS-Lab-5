5f43b6ac2e24068cdb1f98f9e7e048741a8f775b
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-23 15:47:30 -0400

    Enhance docstring for power_of_three function
    Updated docstring for power_of_three function with additional doctests.

--- problem4.py
+++ problem4.py
@@ -1,12 +1,17 @@
 def power_of_three(n):
   """
-  ### add to the docstring, including doctests
+  postive number -> boolean
+  takes an integer and returns True if it is a power of three and False otherwise
 
   >>> power_of_three(27)
   True
   >>> power_of_three(0)
-  ### what should be the answer here?
+  False
   >>> power_of_three(-10)
+  False
+  >>> power_of_three(81)
+  True
+  >>> power_of_three(33)
   False
   """
   ## add your code here
