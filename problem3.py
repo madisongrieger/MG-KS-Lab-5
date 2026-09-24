f13d524ca59252be5a9edfa79dbfcf67e3d0aaf3
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-23 21:56:54 -0400

    Fix syntax errors in roman_to_decimal function

--- problem3.py
+++ problem3.py
@@ -11,30 +11,30 @@ def roman_to_decimal(string):
   3
   """
   result=0
-I=1:
-V=5:
-X=10:
-L=50:
-C=100:
-D=500:
-M=100:
+I=1
+V=5
+X=10
+L=50
+C=100
+D=500
+M=100
 while len(string)>0:
-   if string[0:2]== "IV"
+   if string[0:2]== "IV":
    result= result + 4
    string = string[2:]
- elif string[0:2]== "IX"
+ elif string[0:2]== "IX":
    result= result + 9
    string = string[2:]
-elif string[0:2]== "XL"
+elif string[0:2]== "XL":
    result= result + 40
    string = string[2:]
-elif string[0:2]== "XC"
+elif string[0:2]== "XC":
    result= result + 90
    string = string[2:]
-elif string[0:2]== "CD"
+elif string[0:2]== "CD":
    result= result + 400
    string = string[2:]
-elif string[0:2]== "CM"
+elif string[0:2]== "CM":
    result= result + 900
    string = string[2:]
 return result 
