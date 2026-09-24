8ec61f43c6ff7909c89f9d0abfa6ad9ba78b05cc
madisongrieger <mgrieger01@saintmarys.edu>
2026-09-23 21:55:49 -0400

    Fix variable assignments and indentation in problem3.py
    Corrected syntax for variable assignments and added missing indentation.

--- problem3.py
+++ problem3.py
@@ -11,13 +11,13 @@ def roman_to_decimal(string):
   3
   """
   result=0
-I=1
-V=5
-X=10
-L=50
-C=100
-D=500
-M=100
+I=1:
+V=5:
+X=10:
+L=50:
+C=100:
+D=500:
+M=100:
 while len(string)>0:
    if string[0:2]== "IV"
    result= result + 4
